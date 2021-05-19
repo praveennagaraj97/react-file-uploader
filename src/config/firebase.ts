b4b72d62b4bc3ef0b8e010edfdff813e7b08a011
praveenNagaraj97 <praveenacchusn123@gmail.com>
2021-05-19 22:24:04 +0530

    firebase setup

---
+++ src/config/firebase.ts
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGESENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+export const fireApp = firebase.initializeApp(firebaseConfig);
+export const fireStore = fireApp.storage();
